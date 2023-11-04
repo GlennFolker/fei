f9c60384847c9110c30c697f36eb06b7fcc6d4f9
GlennFolker <glenn.folk08@gmail.com>
2023-11-04 14:25:43 +0700

    Oops.

--- a.cpp
+++
@@ -1,6 +0,0 @@
-#include "a.hpp"
-
-int main() {
-    my_stuff<int>::do_stuff();
-    return 0;
-}
--- b.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "a.hpp"
-
-template<> void my_stuff<int>::do_stuff() {
-    std::cout << "Hello, world!" << std::endl;
-}
